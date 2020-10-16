f772ea144fc53a506248594b2e234b2007646494
struct_dhancha <40660648+jashgopani@users.noreply.github.com>
2020-10-16 22:20:23 +0530

    Added counter loop

--- index.php
+++ index.php
@@ -136,7 +136,12 @@
                         echo "<script>alert(\" Click on any of the effects to preview\");</script>";
                        
                     }
-                    
+                    $x = 1;
+
+                    while($x <= 5) {
+                      echo "The number is: $x <br>";
+                      $x++;
+                    }
                 }
             ?>
     </div></center>
